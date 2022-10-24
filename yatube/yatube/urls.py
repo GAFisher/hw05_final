88ed578326a4ded07ccdd176483de5f1adc8636b
Галина Фишер <galinafisher1010@gmail.com>
2022-10-24 18:11:39 +0300

    'done project'

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
@@ -35,4 +34,3 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
-
